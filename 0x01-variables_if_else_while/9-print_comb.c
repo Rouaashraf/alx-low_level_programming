e7a60b38a36108fd33831a4ed49b17354a8380dd
Rouaa <rouaashraff@gmail.com>
2023-09-08 18:35:23 -0700

    a

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,18 @@
 
 int main(void)
 {
-	int digit1, digit2;
+	int digit = 0;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	while (digit <= 9)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		putchar(digit + 48);
+		if (digit != 9)
 		{
-			putchar(digit1 + '0');
-			putchar(digit2 + '0');
-			if (digit1 != 8 || digit2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
+
+		digit++;
 	}
 	putchar('\n');
 	return (0);
