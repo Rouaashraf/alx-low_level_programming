3429d1eb2eede9ad6ba8cd1051cff9e03f1eda69
Rouaa <rouaashraff@gmail.com>
2023-09-08 18:05:02 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number = 48; number < 58; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
