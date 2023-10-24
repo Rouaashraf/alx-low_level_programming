2a2ac70cc82613247897fc6a9cdc83fa22ec4b5d
Rouaa <rouaashraff@gmail.com>
2023-10-24 14:03:31 -0700

    a

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,24 +2,23 @@
 
 /**
  * get_nodeint_at_index - Returns the nth node of a linked list.
- * @head: Pointer to the head node.
- * @index: Index of the node, starting at 0.
+ * @head: Pointer to the head node of the list.
+ * @index: Index of the node to retrieve.
  *
- * Return: The nth node of the list, or NULL if it doesn't exist.
+ * Return: Pointer to the nth node of the list, or NULL if it doesn't exist.
  */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(const listint_t *head, unsigned int index)
 {
+	const listint_t *current = head;
 	unsigned int count = 0;
-	listint_t *current = head;
 
 	while (current != NULL)
 	{
 		if (count == index)
-			return (current);
-
-		current = current->next;
+			return (listint_t *)current;
 		count++;
+		current = current->next;
 	}
 
 	return (NULL);
