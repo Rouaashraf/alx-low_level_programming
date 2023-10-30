acf4d7392bcdc6ef8cc52c6372e3232b5775fe40
Rouaa <rouaashraff@gmail.com>
2023-10-30 15:25:26 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,35 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
+#include "main.h"
+
+#define BUFFER_SIZE 1024
 
 /**
- * error_and_exit - Print error message and exit with the given code
- * @code: Exit code
- * @message: Error message
+ * @brief Prints error message corresponding to error code and exits program.
+ *
+ * @param error_code The error code indicating the type of error.
+ * @param error_message The error message to be displayed.
  */
-void error_and_exit(int code, const char *message)
+void error_and_exit(int error_code, const char *error_message)
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
+	perror(error_message);
+	exit(error_code);
 }
 
 /**
- * cp_file - Copy the content of a file to another file
- * @file_from: Source file name
- * @file_to: Destination file name
+ * @brief Copies the contents of the source file to the destination file.
+ *
+ * @param file_from The path of the source file.
+ * @param file_to The path of the destination file.
  */
+
 void cp_file(const char *file_from, const char *file_to)
 {
 	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
-	char buffer[1024];
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-		error_and_exit(98, "Error: Can't read from file");
+		error_and_exit(99, "Error opening the source file");
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		error_and_exit(99,
+		error_and_exit(99, "Error opening/creating the destination file");
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+
+		if (bytes_written == -1)
+			error_and_exit(99, "Error writing to the destination file");
+	}
+	if (bytes_read == -1)
+		error_and_exit(99, "Error reading the source file");
+
+	if (close(fd_from) == -1)
+		error_and_exit(99, "Error closing the source file");
+
+	if (close(fd_to) == -1)
+		error_and_exit(99, "Error closing the destination file");
+}
